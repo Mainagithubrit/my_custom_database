15c7b23c84ac48757d7a623fb1153fb78075a9e9
Mainagithubrit <njorogefrancismaina@gmail.com>
2026-01-17 12:29:59 +0300

    database file complete

--- engine/database.py
+++ engine/database.py
@@ -52,14 +52,43 @@ class SimpleDB:
                     row_dict = dict(zip(table.columns, values))
                     return table.insert(row_dict)
                 return f"Error: Table '{table_name}' not found."
-
+        # Logic for 'JOIN' "SELECT FROM table1 JOIN table2 ON table1.id"
+        if "join" in query.lower():
+            return self.handle_join(query)
 
         # Logic for 'SELECT * FROM table_name'
         if query.startswith("select"):
             # Regex to find the table name
             match = re.search(r"from\s+(\w+)", query)
             if match:
-                table_match = match.group(1)
+                table_name = match.group(1)
                 if table_name in self.tables:
                     return self.tables[table_name].select()
                 return f"Error: Table '{table_name}' not found."
+
+        return "Command not recognized"
+
+    def handle_join(self, query):
+       """Links two tables together by comparing values"""
+        # A regex that looks fro the table names and the columns to match on 
+
+        match = re.search(r"from (\w+) join (\w+) on (\w+)\.(\w+) = (\w+)", query, re.IGNORECASE)
+        if not match:
+            return "Join Error: User format 'FROM t1 JOIN t2 ON t1.col = t2.col'"
+        
+        t1_name, t2_name, _, t1_col, _, t2_col = match.groups()
+
+        if t1_name in self.tables and t2_name in self.tables:
+            date1 = self.tables[t1_name].select()
+            data2 = self.tables[t2_name].select()
+
+            combined = []
+
+            # 'Nested Loop that compare every row of t1 to every row of T2'
+            for r1 in data1:
+                for r2 in data2:
+                    if str(r1.get(t1_col)) == str(r2.get(t2_col)):
+                        # create a new dictionary containing data from both 
+                        combined.append({**r1, **r2})
+            return combined
+        return "Error: One or both tables not found"
